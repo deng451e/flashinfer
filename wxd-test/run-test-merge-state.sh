540393ca7963d61694b21f964c11d05c2d28164b
wxd <deng451e@gmail.com>
2024-09-23 01:05:39 +0000

    A6000

--- wxd-test/run-test-merge-state.sh
+++ wxd-test/run-test-merge-state.sh
@@ -1,4 +1,5 @@
-
+rm ${PWD}/bench_mergeState_zeroCopy_a6000.log
+rm ${PWD}/bench_mergeState_a6000.log
 export CUDA_VISIBLE_DEVICES=1
 ${PWD}/../build/bench_all_mergeState_ZeroCopy_methods > ${PWD}/bench_mergeState_zeroCopy_a6000.log
-${PWD}/../build/bench_all_mergeState_methods > ${PWD}/bench_mergeState_zeroCopy_a6000.log
+${PWD}/../build/bench_all_mergeState_methods > ${PWD}/bench_mergeState_a6000.log
