1b37a8eda3b08c4e67305e45c0423d414c61779e
deng451e <deng451e@gmail.com>
2024-10-03 15:56:17 -0500

    rtx4090

--- wxd-test/run-test-mix-attention-single.sh
+++ wxd-test/run-test-mix-attention-single.sh
@@ -6,9 +6,10 @@ do
        
         for q_len in 1 10 50 100 
         do  
-            for ratio in  0.1
+            for ratio in  0 0.01 0.1 0.2 0.5 1
             do 
-            CMD="--arch_name $arch_name --seq_len $seq_len --q_len $q_len --ratio $ratio"
+            CMD="test-mix-attention-single.py --arch_name $arch_name --seq_len $seq_len --q_len $q_len --ratio $ratio"
+            
             output=$(python   $CMD   2>&1 )  
             echo "$outpt" | tee -a mix-attention-single.log
             done
