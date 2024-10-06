b39f017103a2041ba9c8f8efc12d6212ff8ad847
wxd <deng451e@gmail.com>
2024-10-06 02:08:17 +0000

    a6000

---
+++ wxd-test/test-dtype-efficiency.py
@@ -0,0 +1,57 @@
+import torch 
+import time 
+import argparse
+import numpy as np 
+
+def main(args,log):
+    if args.dtype == 'fp16': type_ = torch.float16
+    if args.dtype == 'fp32': type_ = torch.float32
+    if args.device == 'cpu': device = 'cpu'
+    if args.device == 'gpu': device = 'cuda'
+    
+    # warmup
+    log += "==================================\n"
+    log += f"device:{device}, dtype:{type_}\n"
+        
+    for power in range(1,16):
+        dim = 2**power
+        x = torch.randn(dim,dim,dtype=type_) 
+        y = torch.randn(dim,dim,dtype=type_) 
+        if device=='gpu':
+            x = x.to(device)
+            y = y.to(device)
+        for _ in range(3):
+            _ = x@y
+         
+    for power in range(1,16):
+        dim = 2**power  
+        compute_times = []
+        transfer_times = []
+        x = torch.randn(dim,dim,dtype=type_) 
+        y = torch.randn(dim,dim,dtype=type_) 
+        for _ in range(args.repeat):
+            
+            
+            
+            if device=='gpu':
+                st = time.time()
+                x_ = x.to(device)
+                y_ = y.to(device)
+                transfer_times.appnd(time.time()-st)
+            st = time.time()
+            _ = x_@y_
+            compute_times.append(time.time()-st)
+        if device=='cpu': log += f"dim:{dim}, compute time:{np.mean(compute_times)} sec \n"
+        if device=='cpu': log += f"dim:{dim}, compute time:{np.mean(compute_times)} sec, transfer time{np.mean(transfer_times)} sec \n"
+
+    print(log)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dtype", type=str, default="fp16") 
+    parser.add_argument("--device", type=str, default="cpu") 
+    parser.add_argument("--repeat", type=int, default=5)
+    args = parser.parse_args()
+    
+    main(args,"")
